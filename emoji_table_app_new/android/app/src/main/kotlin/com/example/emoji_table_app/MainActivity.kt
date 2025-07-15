84af2410606634cd4edbe98919c605133b1f5f5e
Ashish <a.kumar@emb.email>
2025-07-15 10:12:03 +0530

    Add regenerated android directory with AndroidManifest.xml for emoji_table_app_new

--- emoji_table_app_new/android/app/src/main/kotlin/com/example/emoji_table_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.emoji_table_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- emoji_table_app_new/android/app/src/main/kotlin/com/example/emoji_table_app_new/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.emoji_table_app_new
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
